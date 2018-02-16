381c72a6a6cb64125891cd03f0f6c1bb1ce6a173
Drew Thompson <d_thompson6@u.pacific.edu>
2018-02-15 19:32:03 -0800

    Added customers.js and routes.js

---
+++ acid/src/routes/routes.js
@@ -0,0 +1,26 @@
+const Customer = require('./models/customer');
+const path = require('path');
+const customersRouter = require('./customers');
+
+module.exports = function(app) {
+
+  // server routes ===========================================================
+  // handle things like api calls
+
+  // authentication routes
+  app.use('/customers', customersRouter);
+
+  // frontend routes =========================================================
+
+  app.get('/views/:name', (req, res, next) =>{
+    res.render('views/' + req.params.name);
+  });
+
+  // route to handle all angular requests
+  app.get('*', function(req, res) {
+    res.render('index.pug', {
+      root: path.join(__dirname, '../')
+    });
+  });
+
+};
