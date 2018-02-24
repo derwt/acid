a09357b3b37f84e878982a7b8c85bd570bdb1a8e
Drew Thompson <d_thompson6@u.pacific.edu>
2018-02-24 13:38:07 -0800

    Adding beginnings of customer-icon: responsive icon represents customer data

--- acid/src/app/customer-icon/customer-icon.component.ts
+++ acid/src/app/customer-icon/customer-icon.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Customer } from '../customer';
+
 
 @Component({
   selector: 'customer-icon',
@@ -8,9 +10,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CustomerIconComponent implements OnInit {
 
   @Input() private iconType: string;
+  @Input() private customer: Customer;
+  
+  iconTitle: string;
+  iconInfo: string;
+
   options: string[];
   iconTitles: string[];
-  iconTitle: string;
+
 
   constructor() { }
 
@@ -22,10 +29,15 @@ export class CustomerIconComponent implements OnInit {
       'perm_identity', 'phone', 'location_city', 'location_on', 'map', 
       'description', 'person_pin', 'email', 'access_time', 'create' ];
     this.setIconTitle(this.iconType);
+    this.setIconInfo(this.iconType);
   }
 
   setIconTitle(type: string) {
     this.iconTitle = this.iconTitles[this.options.indexOf(type)];
+  }
+
+  setIconInfo(type: string) {
+    this.iconInfo = type;
   }
 
 }
