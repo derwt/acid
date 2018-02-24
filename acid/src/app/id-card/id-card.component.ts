84479d09f0d830aa27e464510bd759e00d438f84
Drew Thompson <d_thompson6@u.pacific.edu>
2018-02-24 13:37:35 -0800

    Adding beginnings of id-card, with mock data

--- acid/src/app/id-card/id-card.component.ts
+++ acid/src/app/id-card/id-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Customer } from '../customer';
 
@@ -10,19 +10,41 @@ import { Customer } from '../customer';
 
 export class IdCardComponent implements OnInit {
 
+  @Input() private searchType: string;
+
   customer: Customer = {
     cid: 100,
     phone: [6501234444, 1112223333],
     city: "BL",
     address: "2700 Yosemite Drive",
     cross: "Ralston Ave",
+    email: "michael@pizza.face",
     ordered: 10000002,
-    created: 902900500000
+    created: 902900500000,
+    type: 'Personal'
   };
+
+  accountIconTitle: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.setAccountIconTitle(this.customer.type);
+  }
+
+  setAccountIconTitle(type: string) {
+    switch (type) {
+      case 'Personal':
+        this.accountIconTitle = type.slice(0, -2).toLowerCase();
+        break;
+      case 'Business':
+        this.accountIconTitle = type.toLowerCase();
+        break;
+      default:
+        this.customer.type = 'Personal';
+        this.setAccountIconTitle(this.customer.type);
+        break;
+    }
   }
 
 }
