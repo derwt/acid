7fb3752c35b0dd5d9a8f717073231e86564ec74d
Drew Thompson <d_thompson6@u.pacific.edu>
2018-02-25 19:42:33 -0800

    Added crude, but global, phone pipe!!

--- acid/src/app/app.module.ts
+++ acid/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { CustomerService } from './customer.service';
 import { IdCardComponent } from './id-card/id-card.component';
 import { CustomerIconComponent } from './customer-icon/customer-icon.component';
 
+import { PhonePipe } from '../pipes/phone.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     IdCardComponent,
-    CustomerIconComponent
+    CustomerIconComponent,
+    PhonePipe
   ],
   imports: [
     BrowserModule,
---
+++ acid/src/pipes/phone.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({ name: 'phone' })
+export class PhonePipe implements PipeTransform {
+    transform(telephone: number) {
+        let phone = telephone.toString();
+        let formatted = '';
+        for (let i = 0; i < phone.length; i++) {
+            switch (i) {
+                case 0:
+                    formatted += '(';
+                    break;
+                case 3:
+                    formatted += ') ';
+                    break;
+                case 6:
+                    formatted += '-';
+                    break;
+                default:
+                    break;
+            }
+            formatted += phone[i];
+        }
+        return formatted;
+    }
+}
