c0f76a50a57da32c0bd1d3e7657f916d580ec84d
Drew Thompson <d_thompson6@u.pacific.edu>
2018-02-26 22:12:06 -0800

    Added tooltip prototype for id-cards

--- acid/src/app/customer-icon/customer-icon.component.ts
+++ acid/src/app/customer-icon/customer-icon.component.ts
@@ -11,33 +11,46 @@ export class CustomerIconComponent implements OnInit {
 
   @Input() private iconType: string;
   @Input() private customer: Customer;
-  
+
   iconTitle: string;
-  iconInfo: string;
 
   options: string[];
   iconTitles: string[];
 
+  isPhoneIcon: boolean;
 
   constructor() { }
 
   ngOnInit() {
     this.options = [
-      'cid', 'phone', 'city','address', 'cross', 
-      'note', 'name', 'email', 'ordered', 'created' ];
+      'cid', 'phone', 'city', 'address', 'cross',
+      'note', 'name', 'email', 'ordered', 'created'];
     this.iconTitles = [
-      'perm_identity', 'phone', 'location_city', 'location_on', 'map', 
-      'description', 'person_pin', 'email', 'access_time', 'create' ];
+      'perm_identity', 'phone', 'location_city', 'location_on', 'map',
+      'description', 'person_pin', 'email', 'access_time', 'create'];
     this.setIconTitle(this.iconType);
-    this.setIconInfo(this.iconType);
+    this.isPhoneIcon = this.iconType == 'phone';
   }
 
   setIconTitle(type: string) {
     this.iconTitle = this.iconTitles[this.options.indexOf(type)];
   }
 
-  setIconInfo(type: string) {
-    this.iconInfo = type;
+  public getPhonePosition() {
+    switch (this.customer.phone.length) {
+      case 1:
+        return 'translate(-15px, 11%)';
+      case 2:
+        return 'translate(-15px, 47%)';
+      case 3:
+        return 'translate(-15px, 65%)';
+      case 4:
+        return 'translate(-15px, 79px)';
+      case 5:
+        return 'translate(-15px, 76%)';
+      default:
+        break;
+    }
   }
 
 }
