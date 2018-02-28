26b51f850a3b43456aef73d94f9e5149dadddab2
Drew Thompson <d_thompson6@u.pacific.edu>
2018-02-27 17:24:53 -0800

    Refactored CUSTOMERS test data to ts standards

--- acid/src/app/CUSTOMERS.ts
+++ acid/src/app/CUSTOMERS.ts
@@ -1,13 +1,13 @@
 import { Customer } from './customer';
 
-export var CUSTOMERS = [
+export const CUSTOMERS = [
     {
         cid: 100,
         phone: [6501234444, 1112223333],
-        city: "BL",
-        address: "2700 Yosemite Drive",
-        cross: "Ralston Ave",
-        email: "michael@pizza.face",
+        city: 'BL',
+        address: '2700 Yosemite Drive',
+        cross: 'Ralston Ave',
+        email: 'michael@pizza.face',
         ordered: 10000002,
         created: 902900500000,
         type: 'Personal'
@@ -15,8 +15,10 @@ export var CUSTOMERS = [
     {
         cid: 101,
         phone: [6506785226, 3332221111, 3333334444],
-        address: "900 Place Avenue",
-        email: "play@li.chess",
-        name: 'Nerd'
+        // address: '900 Place Avenue',
+        email: 'play@li.chess',
+        created: 1519778187,
+        name: 'Nerd',
+        type: 'Business'
     }
-]
+];
