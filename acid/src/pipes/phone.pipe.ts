177ce050e6e2ff69f8e5c2255423c06010b43d04
Drew Thompson <d_thompson6@u.pacific.edu>
2018-05-05 14:47:46 -0700

    Updated phone pipe to return blank on undefined input

--- acid/src/pipes/phone.pipe.ts
+++ acid/src/pipes/phone.pipe.ts
@@ -3,7 +3,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'phone' })
 export class PhonePipe implements PipeTransform {
     transform(telephone: number) {
-        let phone = telephone.toString();
+        if (!telephone) {
+            return;
+        }
+        const phone = telephone.toString();
         let formatted = '';
         for (let i = 0; i < phone.length; i++) {
             switch (i) {
